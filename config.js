ac7523022f440d3a0e835aac4c12883c458c660c
redfyu <rose.udie0420@gmail.com>
2026-09-15 20:26:12 +0100

    Update configuration details in config.js

--- config.js
+++ config.js
@@ -14,35 +14,35 @@
  * with prior permission and proper credit.
  *
  * OFFICIAL LINKS (ONLY):
- * YouTube   : https://youtube.com/@voidsec7718
- * Instagram : sabir._7718
+ * YouTube   : https://youtube.com/@automationlordd
+ * Instagram : automationlordd
  * Telegram  : https://t.me/SABIR7718
- * GitHub    : https://github.com/SABIR7718
- * WhatsApp  : +91 73650 85213
+ * GitHub    : https://github.com/redfyu
+ * WhatsApp  : +2348112370330
  *
  * Violations may result in DMCA takedown
  * or termination of the Telegram bot.
  */
 module.exports = {
-    mainToken: 'BOT_TOKEN', // Bot Token Here 
+    mainToken: '8774329879:AAHR15hhhmuj2WulvjnZlfhAyUrvggV_tXQ', // Bot Token Here 
     
-    S7: '@SeXyxeon13', // Owner, Name
+    S7: '@mrhacccker', // Owner, Name
     
-    adminId: 'CHAT_ID', // Owner Chat ID ( control /address, /delresell )
+    adminId: '7185511617', // Owner Chat ID ( control /address, /delresell )
     
-    channel: 'TG_CHANNEL', // TG channel link
+    channel: 'mrhackerotp', // TG channel link
     
-    group: 'GC_LINK', // TG group link
+    group: 'mrhackerotp', // TG group link
     
-    youtube: 'YOITUBE_LINK', // YouTube Link
+    youtube: 'https://youtube.com/@automationlordd', // YouTube Link
     
-    waChannel: 'WA_LINK', // WhatsApp Channel Link
+    waChannel: 'https://wa.me/tgui', // WhatsApp Channel Link
     
-    instagram: 'INSTA_LINK', // Instagram Link 
+    instagram: 'automationlordd', // Instagram Link 
     
-    channelId: 'CHANNEL_ID', // channel ID bot must be admin in the channel
+    channelId: '-1004435991978', // channel ID bot must be admin in the channel
     
-    groupId: 'GROUP_ID', // group ID bot must be admin in the group
+    groupId: '-1003729345841', // group ID bot must be admin in the group
     
     bot: 'Xeon Bug Bot V27', // Bot name 
     
